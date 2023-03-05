149ef9f13a7218b8cf842ba7b64b5f2730200a3c
darkotica <58399701+darkotica@users.noreply.github.com>
2023-03-05 15:29:26 +0100

    Created script for face detection, passed smoke detection test. Refactored hands_detection, and added function for calculation of lower bound for hands.

--- hands_detection.py
+++ hands_detection.py
@@ -1,44 +1,75 @@
 import mediapipe as mp
-from PIL import Image
-from mediapipe import ImageFormat
-from numpy import asarray
 import cv2
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(
-    static_image_mode=True,
-    model_complexity=1,
-    min_detection_confidence=0.75,
-    min_tracking_confidence=0.75,
-    max_num_hands=2)
+def get_hand_landmarks_from_image(image_path): # './test_images/face_image_1.jpg'
+    mp_hands = mp.solutions.hands
 
-image = cv2.imread('./test_images/hand_image_1.png')
+    hands = mp_hands.Hands(
+        static_image_mode=True,
+        model_complexity=1,
+        min_detection_confidence=0.75,
+        min_tracking_confidence=0.75,
+        max_num_hands=2)
 
-results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    image = cv2.imread(image_path)
+    image = cv2.flip(image, 1)
 
-if results.multi_hand_landmarks:
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    return results.multi_hand_landmarks
+
+
+def draw_hand_landmarks_from_image(image_path, hand_landmarks,
+                                   output_image_path):
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    image = cv2.imread(image_path)
+    image = cv2.flip(image, 1)
+
     image_height, image_width, _ = image.shape
     annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-        print('hand_landmarks:', hand_landmarks)
-        print(
-            f'Index finger tip coordinates: (',
-            f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-            f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-        )
+    for hand_landmarks in hand_landmarks:
         mp_drawing.draw_landmarks(
             annotated_image,
             hand_landmarks,
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imwrite(
-        './test_images/annotated_image_0.png',
-        cv2.flip(annotated_image, 1))
 
-    for hand_world_landmarks in results.multi_hand_world_landmarks:
-        mp_drawing.plot_landmarks(
-            hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+    cv2.imwrite(output_image_path, cv2.flip(annotated_image, 1))
+
+
+def get_lower_bound_hands_pixels(image_path):
+    mp_hands = mp.solutions.hands
+    hands = get_hand_landmarks_from_image(image_path)
+
+    image = cv2.imread(image_path)
+    image = cv2.flip(image, 1)
+
+    image_height, image_width, _ = image.shape
+    print(image.shape)
+    lower_bound_pixels = []
+    for hand_landmarks in hands:
+        wrist_pos = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
+        print(wrist_pos)
+        lower_bound_y = wrist_pos.y * image_height
+        lower_bound_pixels.append(lower_bound_y)
+
+        # cv2.line(image, (0, int(lower_bound_y)),
+        #          (image_width, int(lower_bound_y)), (0, 255, 0),
+        #          thickness=2)
+
+    #cv2.imwrite("./test_images/hand_line.png", cv2.flip(image, 1))
+
+    return lower_bound_pixels
+
+
+# print(get_lower_bound_hands_pixels("./test_images/hand_image_1.png"))
+# draw_hand_landmarks_from_image("./test_images/hand_image_1.png",
+#                                get_hand_landmarks_from_image("./test_images/"
+#                                                              "hand_image_1."
+#                                                              "png"),
+#                                "./test_images/hand_annotated_image_0.png")
